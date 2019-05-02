2a8f55e1e07ffdb82d5d735010056fa1510626cc
Michael Putterflam <michael.putterflam@gmail.com>
2019-05-02 14:34:13 -0700

    data posts when clicking submit button

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,15 @@
 var friendData = require("../data/friends");
 
-module.exports = function(app){
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res){
+    // API GET Route
+    app.get("/api/friends", function (req, res) {
         res.json(friendData);
     });
 
+    // API POST Route
+    app.post("/api/friends", function (req, res) {
+        var newFriend = req.body;
+        console.log(newFriend);
+    });
 };
